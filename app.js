2df51b808c54327eeb547e4ffab61a70b93e30a9
Runner-Dad84 <vanhoogie@gmail.com>
2025-06-04 20:56:36 -0400

    remove unused code

--- app.js
+++ app.js
@@ -10,19 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/", newRouter);
 
-
-const links = [
-    { href: "/", text: "Home" },
-    { href: "/messages", text: "Messages" }
-];
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
-
-app.get("/", (req, res) => {
-    res.render("index", { links: links, messages: messages });
-  });
 
 const PORT = 3012;
 app.listen(PORT, () => {
